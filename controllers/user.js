cae520101eab1143d3a57b2d6fb99178b9c3af03
gyorutan <waou1998@gmail.com>
2023-05-19 20:57:18 +0900

    UPDATE

--- controllers/user.js
+++ controllers/user.js
@@ -6,8 +6,18 @@ exports.getUserProfile = async (req, res) => {
   try {
     const { id } = req.params;
     const user = await User.findById(id);
-    const individual = await Individual.find({ user: id });
-    const band = await Band.find({ user: id });
+    const individual = await Individual.find({ user: id })
+      .sort({ date: 1, time1: 1 })
+      .populate({
+        path: "user",
+        select: "username",
+      });
+    const band = await Band.find({ user: id })
+      .sort({ date: 1, time1: 1 })
+      .populate({
+        path: "user",
+        select: "username",
+      });
     return res.status(200).json({ user, individual, band });
   } catch (error) {
     console.log(error);
